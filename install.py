4f8ccae861472a5d2f46256f0c56ad042642aa06
fredwei1031 <47200165+fredwei1031@users.noreply.github.com>
2025-11-11 21:42:25 +0800

    Update install.py

--- install.py
+++ install.py
@@ -11,8 +11,8 @@ import sys
 import getopt
 import filecmp
 
-openresty_pkg_url = 'https://openresty.org/download/openresty-1.21.4.3.tar.gz' 
-openresty_pkg = 'openresty-1.21.4.3.tar.gz'
+openresty_pkg_url = 'https://openresty.org/download/openresty-1.21.4.4.tar.gz' 
+openresty_pkg = 'openresty-1.21.4.4.tar.gz'
 
 work_path = os.getcwd()
 
